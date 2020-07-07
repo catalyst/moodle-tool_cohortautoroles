8de63e64a6832735ae34c4f0062adec121ebe52c
Ian Wild <ian.wild@aveva.com>
2020-07-07 15:20:19 +0100

    Update version number.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2019071500; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2020070700; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016051900; // Requires this Moodle version.
 $plugin->component = 'tool_cohortautoroles'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
