9d7542a9e0f30f96045f864e735b270390fdd176
Ian Wild <ian.wild@aveva.com>
2020-07-08 12:36:34 +0100

    Update required version as cohort themes were implemented in Moodle .3.5.x

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 
 $plugin->version   = 2020070700; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2016051900; // Requires this Moodle version.
+$plugin->requires  = 2018051700; // Requires this Moodle version.
 $plugin->component = 'tool_cohortautoroles'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = "1";
